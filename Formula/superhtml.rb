55a4c317f31fc2dd91ebd88aab55a6d34049dd70
Boyan Xu <boyan.xu@requo.in>
2024-11-19 11:26:26 +0000

    fix: update URL for ARM architecture in superhtml formula
    Signed-off-by: Boyan Xu <boyan.xu@requo.in>

--- Formula/superhtml.rb
+++ Formula/superhtml.rb
@@ -6,7 +6,7 @@ class Superhtml < Formula
   on_macos do
     if Hardware::CPU.arm?
       sha256 "b8b2327f666ff316422061284e107add5c413ebdfdb91774c0c3702a66e65ec9"
-      url "https://github.com/kristoff-it/superhtml/releases/download/v#{version}/arm64-macos.tar.gz"
+      url "https://github.com/kristoff-it/superhtml/releases/download/v#{version}/aarch64-macos.tar.gz"
     else
       sha256 "e171f080e7464afe0717bbe45894e8558702d054421d21e0fa5e5c4ee95e85c3"
       url "https://github.com/kristoff-it/superhtml/releases/download/v#{version}/x86_64-macos.tar.gz"
